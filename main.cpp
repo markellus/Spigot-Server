1306bb4f203c07a058d694f61bcf53259e08dd09
Markellus <gmx@marcel-bulla.de>
2019-06-30 18:17:23 +0200

    The buffer size of the shared memory is no longer an issue

--- main.cpp
+++ main.cpp
@@ -22,12 +22,92 @@
 
 #pragma comment(lib, "spgt-lib.lib")
 
+const int CONTENT_SIZE = 4096;
+
 struct SM
 {
 	int iStatus;
-	int iArgc;
-	char cContent[4096];
+	int iContentSizeMax = CONTENT_SIZE;
+	int iContentSize;
+	char cContent[CONTENT_SIZE];
 };
+
+enum Status
+{
+	EXIT = -1,
+	RECEIVE_DATA = 100,
+	RECEIVE_DATA_PART = 101,
+	RECEIVE_DATA_RECEIVED = 102,
+	SEND_DATA = 200,
+	SEND_DATA_PART = 201,
+	SEND_DATA_RECEIVED = 202
+};
+
+void SendData(SM* sm, HANDLE mutex, std::string strData)
+{
+	unsigned int iPos = 0;
+	unsigned int iDataSize = strData.size();
+	const char* data = strData.c_str();
+
+	while(iPos != iDataSize)
+	{
+		WaitForSingleObject(mutex, INFINITE);
+
+		if (sm->iStatus == RECEIVE_DATA_RECEIVED)
+		{
+			if (CONTENT_SIZE > iDataSize - iPos)
+			{
+				sm->iContentSize = iDataSize - iPos;
+				memcpy(sm->cContent, data + sizeof(char) * iPos, iDataSize - iPos);
+				sm->iStatus = RECEIVE_DATA;
+				iPos += iDataSize - iPos;
+			}
+			else
+			{
+				iPos += CONTENT_SIZE;
+				sm->iContentSize = CONTENT_SIZE;
+				memcpy(sm->cContent, data + sizeof(char) * iPos, CONTENT_SIZE);
+				sm->iStatus = RECEIVE_DATA_PART;
+			}
+		}
+
+		ReleaseMutex(mutex);
+	}
+}
+
+char* ReceiveData(SM* sm, HANDLE mutex)
+{
+	bool bReceived = false;
+
+	std::string strResult;
+
+	while(!bReceived)
+	{
+		WaitForSingleObject(mutex, INFINITE);
+
+		if (sm->iStatus != SEND_DATA_RECEIVED)
+		{
+			char* cPart = new char[sm->iContentSize + 1];
+			memcpy(cPart, sm->cContent, sm->iContentSize);
+			cPart[sm->iContentSize] = '\0';
+			strResult += cPart;
+
+			if (sm->iStatus == SEND_DATA)
+			{
+				bReceived = true;
+			}
+
+			sm->iStatus = SEND_DATA_RECEIVED;
+		}
+
+		ReleaseMutex(mutex);
+	}
+
+	char* cResult = new char[strResult.size()];
+	memcpy(cResult, strResult.c_str(), strResult.size() + 1);
+
+	return cResult;
+}
 
 int main(int argc, char** argv)
 {
@@ -40,28 +120,28 @@ int main(int argc, char** argv)
 		fm = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, sizeof(SM), L"SPGT_SM");
 		sm = static_cast<struct SM*>(MapViewOfFile(fm, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, sizeof(SM)));
 		sm->iStatus = 0;
+		sm->iContentSizeMax = CONTENT_SIZE;
 	}
 	catch (...)
 	{
 		return -2;
 	}
 
-	while(sm->iStatus != -1)
+	while(sm->iStatus != EXIT)
 	{
-		WaitForSingleObject(mutex, INFINITE);
-		if (sm->iStatus == 2)
+		if (sm->iStatus == SEND_DATA || sm->iStatus == SEND_DATA_PART)
 		{
-			char* cContent[2];
-			cContent[1] = sm->cContent;
-			std::string strOut = Call(sm->iArgc + 1, cContent);
-			memcpy(sm->cContent, strOut.c_str(), strOut.size());
-			sm->iStatus = 1;
+			char* cContent[3];
+			cContent[1] = "-d 600\0";
+			cContent[2] = ReceiveData(sm, mutex);
+			std::string strOut = Call(3, cContent);
+			SendData(sm, mutex, strOut);
 		}
 		else
 		{
-			Sleep(1);
+			WaitForSingleObject(mutex, INFINITE);
+			ReleaseMutex(mutex);
 		}
-		ReleaseMutex(mutex);
 	}
 	UnmapViewOfFile(sm);
 	CloseHandle(fm);
